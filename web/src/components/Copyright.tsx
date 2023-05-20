594cc50925044cec155135254c8caf1568b21990
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-05-20 13:55:21 -0300

    feat: create authentication flow in web

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+const Copyright = () => {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        href="https://rocketseat.com.br"
+        className="underline transition duration-300 hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  );
+};
+
+export default Copyright;
---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,18 @@
+const EmptyMemories = () => {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a
+          href=""
+          className="underline transition-all duration-300 hover:text-gray-50"
+        >
+          criar agora
+        </a>
+        !
+      </p>
+    </div>
+  );
+};
+
+export default EmptyMemories;
