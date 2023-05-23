f7af04b5a63c8fff81ae9055bd29c97f8e16a361
Eduardo Varela Sousa <eduardo.varela@outlook.com.br>
2023-05-23 12:21:19 -0300

    fix: api route

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import 'dotenv/config';
-
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import jwt from '@fastify/jwt';
 import multipart from '@fastify/multipart';
-import staticModel from '@fastify/static';
-import { memoriesRoute } from './routes/memories';
+import { memoriesRoutes } from './routes/memories';
 import { authRoutes } from './routes/auth';
 import { uploadRoutes } from './routes/upload';
 import { resolve } from 'node:path';
@@ -14,22 +13,21 @@ const app = fastify();
 
 app.register(multipart);
 
-app.register(staticModel), {
+app.register(require('@fastify/static'), {
     root: resolve(__dirname, '../uploads'),
-    prefix: '/uploads'
-};
-
-app.register(cors, {
-    origin: ['http://localhost:3000'],
+    prefix: '/uploads',
 });
 
+app.register(cors, {
+    origin: true,
+});
 app.register(jwt, {
     secret: 'spacetime',
 });
 
 app.register(authRoutes);
 app.register(uploadRoutes);
-app.register(memoriesRoute);
+app.register(memoriesRoutes);
 
 app
     .listen({
@@ -37,5 +35,5 @@ app
         host: '0.0.0.0',
     })
     .then(() => {
-        console.log('🚀 HTTP server running on http://localhost:3333');
+        console.log('🚀 HTTP server running on port http://localhost:3333');
     });
